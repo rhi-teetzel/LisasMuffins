f518383eb615ef59a6d32553d9a9e5418141121f
rhi-teetzel <rhiannateetzel@gmail.com>
2020-01-15 13:09:38 -0500

    initial commit Lisas Muffins

---
+++ LisasMuffins/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LisasMuffins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Initialize Variables
+            int cupsOfSugarRecipe = 7;
+            int currentCupsOfSugar = 2;
+            int remainingCupsRequired = 0;
+
+            //Variable Computation
+            remainingCupsRequired = cupsOfSugarRecipe - currentCupsOfSugar;
+
+            //Variable Output
+            Console.WriteLine("Lisa still needs {0} cups", remainingCupsRequired);
+
+            Console.ReadKey();
+
+        }
+            
+    }
+}
